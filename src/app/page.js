2e7a8f7eae48e7057ebbac76c68ea0a111784735
gaurav kumar <gaurav.k7303@gmail.com>
2024-07-27 11:58:12 +0530

    this is some new changes

--- src/app/page.js
+++ src/app/page.js
@@ -10,15 +10,17 @@ export default function Home() {
   const [messages, setMessages] = useState([]);
   const [userInput, setUserInput] = useState("");
   const [chat, setChat] = useState(null);
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
   const [error, setError] = useState(null);
+  const [showWelcome, setShowWelcome] = useState(true);
+  const [isTyping, setIsTyping] = useState(false);
 
   const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
   const MODEL_NAME = "gemini-1.0-pro-001";
 
-  const chatContainerRef = useRef(null); // Reference for chat container
+  const chatContainerRef = useRef(null);
 
-  console.log("API_KEY:", API_KEY); // Debugging: Check if API key is being access
+  console.log("API_KEY:", API_KEY); // Debugging: Check if API key is being accessed
 
   const genAI = new GoogleGenerativeAI(API_KEY);
 
@@ -52,7 +54,13 @@ export default function Home() {
     "What services are available for elderly care?": "Services available for elderly care include home healthcare, assisted living, adult day care, and hospice care.",
     "How can I find a reliable caregiver?": "You can find a reliable caregiver through agencies, recommendations from friends or family, and online platforms with verified reviews.",
     "What financial assistance is available for elderly care?": "Financial assistance for elderly care can include Medicaid, Medicare, veteran benefits, and long-term care insurance.",
-    "How do I know if my elderly loved one needs professional care?": "Signs that professional care might be needed include difficulty with daily activities, worsening health conditions, and safety concerns at home."
+    "How do I know if my elderly loved one needs professional care?": "Signs that professional care might be needed include difficulty with daily activities, worsening health conditions, and safety concerns at home.",
+    "What are the signs of elder abuse?": "Signs of elder abuse include unexplained injuries, withdrawal from normal activities, sudden changes in financial situation, and poor hygiene.",
+    "How can I improve the quality of life for my elderly loved one?": "Improving quality of life can include social engagement, physical activities, proper nutrition, and mental stimulation.",
+    "What are the benefits of assisted living facilities?": "Benefits of assisted living facilities include personalized care, social activities, safety, and a supportive community.",
+    "How can I help my elderly loved one stay mentally active?": "Encourage them to engage in activities like reading, puzzles, socializing, and learning new skills or hobbies.",
+    "What are the options for end-of-life care?": "End-of-life care options include palliative care, hospice care, and home care, focusing on comfort and quality of life.",
+    "How can I support a family member with dementia?": "Support can include creating a safe environment, establishing routines, providing emotional support, and seeking professional help when needed."
   };
 
   useEffect(() => {
@@ -86,28 +94,58 @@ export default function Home() {
 
       setMessages((prevMessages) => [...prevMessages, userMessage]);
       setUserInput("");
+      setShowWelcome(false);
 
       if (predefinedResponses[userInput]) {
-        const botMessage = {
-          text: predefinedResponses[userInput],
-          role: "bot",
-          timestamp: new Date(),
-        };
-        setMessages((prevMessages) => [...prevMessages, botMessage]);
+        setIsTyping(true);
+        setTimeout(() => {
+          const botMessage = {
+            text: predefinedResponses[userInput],
+            role: "bot",
+            timestamp: new Date(),
+          };
+          setMessages((prevMessages) => [...prevMessages, botMessage]);
+          setIsTyping(false);
+        }, 2000); // Simulate typing delay
       } else if (chat) {
+        setIsTyping(true);
         const result = await chat.sendMessage(userInput);
-        const botMessage = {
-          text: await result.response.text(),
-          role: "bot",
-          timestamp: new Date(),
-        };
-        setMessages((prevMessages) => [...prevMessages, botMessage]);
+        setTimeout(async () => {
+          const botMessage = {
+            text: await result.response.text(),
+            role: "bot",
+            timestamp: new Date(),
+          };
+          setMessages((prevMessages) => [...prevMessages, botMessage]);
+          setIsTyping(false);
+        }, 2000); // Simulate typing delay
       } else {
         setError("Chat is not initialized");
       }
     } catch (error) {
       setError("Failed to send message: " + error.message);
     }
+  };
+
+  const handlePredefinedResponse = (question) => {
+    const userMessage = {
+      text: question,
+      role: "user",
+      timestamp: new Date(),
+    };
+    setMessages((prevMessages) => [...prevMessages, userMessage]);
+    setShowWelcome(false);
+
+    setIsTyping(true);
+    setTimeout(() => {
+      const botMessage = {
+        text: predefinedResponses[question],
+        role: "bot",
+        timestamp: new Date(),
+      };
+      setMessages((prevMessages) => [...prevMessages, botMessage]);
+      setIsTyping(false);
+    }, 2000); // Simulate typing delay
   };
 
   const handleThemeChange = (e) => {
@@ -175,6 +213,28 @@ export default function Home() {
         </div>
       </div>
       <div ref={chatContainerRef} className={`flex-1 overflow-y-auto p-4 ${secondary}`}>
+        {showWelcome && (
+          <div className="mb-4 main-container m-auto">
+            <div className={`p-4 rounded-lg ${accent} text-white w-4/4 m-auto`}>
+              <h1 className="font-bold text-3xl mb-8">Welcome to the Elderly Care Advisor Chatbot!</h1>
+              <p>This chatbot is designed to provide assistance and information about elderly care. You can ask about services, caregivers, financial assistance, signs of elder abuse, and more.</p>
+              <p>How can I assist you today? Here are some common questions:</p>
+            </div>
+            <div className="flex flex-wrap">
+              <div className="mt-2 space-y-2">
+                {Object.keys(predefinedResponses).map((question, index) => (
+                  <button
+                    key={index}
+                    onClick={() => handlePredefinedResponse(question)}
+                    className="inline-block w-full p-2 rounded-lg bg-gray-200 hover:bg-gray-300 text-black"
+                  >
+                    {question}
+                  </button>
+                ))}
+              </div>
+              </div>
+          </div>
+        )}
         {messages.map((msg, index) => (
           <div
             key={index}
@@ -188,9 +248,8 @@ export default function Home() {
                   className="w-6 h-6"
                 />
                 <span
-                  className={`inline-block p-2 rounded-lg ${
-                    msg.role === "user" ? `${accent} text-white` : `${primary} ${text}`
-                  }`}
+                  className={`inline-block p-2 rounded-lg ${msg.role === "user" ? `${accent} text-white` : `${primary} ${text}`
+                    }`}
                 >
                   {msg.text}
                 </span>
@@ -198,9 +257,8 @@ export default function Home() {
             )}
             {msg.role === "user" && (
               <span
-                className={`inline-block p-2 rounded-lg ${
-                  msg.role === "user" ? `${accent} text-white` : `${primary} ${text}`
-                }`}
+                className={`inline-block p-2 rounded-lg ${msg.role === "user" ? `${accent} text-white` : `${primary} ${text}`
+                  }`}
               >
                 {msg.text}
               </span>
@@ -211,6 +269,20 @@ export default function Home() {
             </p>
           </div>
         ))}
+        {isTyping && (
+          <div className="flex items-center space-x-2 mb-4">
+            <img
+              src="/chatbot.png" // Replace with your logo URL
+              alt="Bot Logo"
+              className="w-6 h-6"
+            />
+            <div className="flex space-x-1">
+              <div className="w-2 h-2 bg-gray-500 rounded-full animate-bounce"></div>
+              <div className="w-2 h-2 bg-gray-500 rounded-full animate-bounce delay-100"></div>
+              <div className="w-2 h-2 bg-gray-500 rounded-full animate-bounce delay-200"></div>
+            </div>
+          </div>
+        )}
         {error && <div className="text-red-500 text-sm mb-4">{error}</div>}
       </div>
       <div className=" bottom-0 left-0 w-full flex items-center p-4">
@@ -220,7 +292,7 @@ export default function Home() {
           value={userInput}
           onChange={(e) => setUserInput(e.target.value)}
           onKeyDown={handleKeyPress}
-          className="flex-1 p-2 border border-gray-300 rounded-full focus:outline-none"
+          className="flex-1 p-2 border text-black border-gray-300 rounded-full focus:outline-none"
         />
         <button
           onClick={handleSendMessage}
@@ -232,4 +304,3 @@ export default function Home() {
     </div>
   );
 }
-
